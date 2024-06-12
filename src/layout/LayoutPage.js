8f163e6de0a5012d4b6acbff23871ead2d16c5fc
tronghoa <hoa.truongtrong26@gmail.com>
2024-06-12 23:31:32 +0700

    feat(6): UI for create new user

---
+++ src/layout/LayoutPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../styles/LayoutPage.css";
+import { Layout } from "antd";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+
+const { Content } = Layout;
+const LayoutPage = (props) => { 
+  return (
+    <Layout className="bg-white w-full flex">
+      <Header/>
+      <Layout className="bg-white mx-5 w-full">
+        <Navbar/>
+        <Layout className="bg-white pt-0 pr-5 pb-2.5 pl-12 h-full">
+          <Content className="flex p-6 m-5 min-h-[280px] h-screen rounded-lg bg-white">
+            {props.children}
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
+  );
+};
+export default LayoutPage;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'd6001c': '#d6001c',
+      },
+    },
+  },
+  plugins: [],
+}
+
