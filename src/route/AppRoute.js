5352b3546f4bd1ae1abc3fdef18c44957fc2c183
Trọng Hòa <124591630+tronghoat47@users.noreply.github.com>
2024-06-13 10:06:27 +0700

    Feat/6 admin create new user (#3)
    * ignore
    
    * feat(6): UI for create new user

--- src/route/AppRoute.js
+++ src/route/AppRoute.js
@@ -1,14 +1,18 @@
-const { useRoutes } = require("react-router-dom")
-const { default: ManageUser } = require("../pages/ManageUser")
+const { useRoutes } = require("react-router-dom");
+const { default: ManageUser } = require("../pages/ManageUser");
+const { default: CreateUser } = require("../pages/CreateUser");
 
 const AppRoutes = () => {
-    const elements = useRoutes(
-        [
-            {
-                path: 'manager-user', element: <ManageUser />
-            },
-        ]
-    )
-    return elements
-}
-export default AppRoutes
+  const elements = useRoutes([
+    {
+      path: "/manager-user/create-user",
+      element: <CreateUser />,
+    },
+    {
+      path: "/",
+      element: <ManageUser />,
+    },
+  ]);
+  return elements;
+};
+export default AppRoutes;
