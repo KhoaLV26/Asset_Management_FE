a2ec003c7fbf6c8613eb01cca759c1147c107009
tronghoa <hoa.truongtrong26@gmail.com>
2024-06-14 15:06:36 +0700

    init

---
+++ src/pages/CreateAsset.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "../styles/CreateAsset.css";
+
+const CreateAsset = () => {
+  const [categoryData, setCategoryData] = useState([]);
+
+  return <div></div>;
+};
+
+export default CreateAsset;
