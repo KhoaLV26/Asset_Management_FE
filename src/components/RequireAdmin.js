0382db71281d5082968490e0eaf0ca12198f0d91
tronghoa <hoa.truongtrong26@gmail.com>
2024-06-18 17:39:35 +0700

    fix(5): fix require login when logout

--- src/components/RequireAdmin.js
+++ src/components/RequireAdmin.js
@@ -3,7 +3,13 @@ import { Navigate } from "react-router-dom";
 const RequireAdmin = (props) => {
   const { children } = props;
   const user = JSON.parse(localStorage.getItem("user"));
-  return user.roleName === `Admin` ? children : <Navigate to="/unauthorized" />;
+  return !user ? (
+    <Navigate to="/login" />
+  ) : user.roleName === `Admin` ? (
+    children
+  ) : (
+    <Navigate to="/unauthorized" />
+  );
 };
 
 export default RequireAdmin;
